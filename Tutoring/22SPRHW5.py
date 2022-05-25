e1ad8bbeff637ca6203082b71175b2c6cb86094a
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-05-26 01:15:07 +0900

    5/26 Tutoring

--- Tutoring/22SPRHW5.py
+++ Tutoring/22SPRHW5.py
@@ -6,17 +6,33 @@
 #       to the index 'to_index'.
 def move(lst, from_index, to_index):
     final = []
-    # 변하지 않는 부분 복사
-    for i in range(to_index):
-        final += [lst[i]]
-    # 옮길 숫자 파이널 리스트에 넣기
-    final += [lst[from_index]]
-    # 옮긴 숫자 제외 나머지 숫자 복사
-    for j in range(to_index, len(lst)):
-        if j == from_index:
-            continue
-        else:
+
+    # 뒤에서 앞으로 + 그대로
+    if from_index >= to_index:
+        # 변하지 않는 부분 복사
+        for i in range(to_index):
+            final += [lst[i]]
+        # 옮길 숫자 파이널 리스트에 넣기
+        final += [lst[from_index]]
+        # 옮긴 숫자 제외 나머지 숫자 복사
+        for j in range(to_index, len(lst)):
+            if j == from_index:
+                continue
+            else:
+                final += [lst[j]]
+    #앞에서 뒤로
+    else:
+        # 변하지 않는 부분 복사
+        for i in range(from_index):
+            final += [lst[i]]
+        for j in range(from_index + 1, to_index + 1):
             final += [lst[j]]
+        final += [lst[from_index]]
+        for k in range(to_index + 1, len(lst)):
+            final += [lst[k]]
+
+
+
     return final
 
 # ---------------------------- Q2 ---------------------------------------
@@ -28,5 +44,10 @@ def move(lst, from_index, to_index):
 # The following lines are for testing purposes, and will not be part of
 # your grade. You may freely modify the following codes.
 lst = [3, 2, 1, 5, 9, 2]
-print(move(lst, 3, 1)) # Should print out [3, 5, 2, 1, 9, 2]
+print(move(lst, 1, 3)) # 3, 1: Should print out [3, 5, 2, 1, 9, 2]
+                       # 1, 3: [3, 1, 5, 2, 9, 2]
+
+ex_list = [1, 2, 3, 4, 5]
+print(move(ex_list, 0, 4))
+
 
--- Tutoring/temp.py
+++ Tutoring/temp.py
@@ -1,4 +1,13 @@
-s = "123"
 
-print(s[1:])
+
+
+
+s = "1234"
+"1"
+for letter in s:
+    boolean = False
+    for k in range(10):
+        if str(k) == letter:
+            boolean = True
+
 
